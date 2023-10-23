b8f74cf7f1492386ca6675cda025f9e27bd3174f
developerkorir <caltonkorir45@gmail.com>
2023-10-23 15:20:19 +0300

    Introduction

---
+++ main.py
@@ -0,0 +1,11 @@
+print(99)
+# variables, data types, math operations, strings
+
+print('Hello Python, this is day 1')
+
+# this is a string
+print("99")
+
+# continued printing using commas
+print("My position is", 99, "and my answer is", True)
+
---
+++ variables.py
@@ -0,0 +1,26 @@
+# A variable is a named storage location
+x = 11
+y = 16
+PI = 22 / 7
+radius = 24
+
+area = PI * (radius * radius)
+print("The area is:", area)
+
+name = "Carlton"
+city = "Nairobi"
+sentence = "The quick brown fox jumped over the lazy fox"
+distance_to_moyale = 750
+
+# use underscores to name variables
+maximum_loan_amount = 10000
+
+# Boolean 
+isConnected = True
+is_student = False
+
+print(name, "is from", city, "and qualifies for", maximum_loan_amount)
+
+# Constants variable names are always all in uppercase
+MAX_WITHDRAWAL_AMOUNT = 60000
+
