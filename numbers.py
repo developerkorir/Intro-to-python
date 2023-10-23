dfeafcdfecad5af65a41a86b63a687cc3ea85017
developerkorir <caltonkorir45@gmail.com>
2023-10-23 15:52:34 +0300

    number manipulation

---
+++ numbers.py
@@ -0,0 +1,24 @@
+num_1 = 100
+num_2 = 15
+num_3 = 10.55
+
+# Basic mathematical operations
+print(num_1+num_2)
+print(num_1-num_2)
+print(num_2*num_3)
+print(num_1/num_2)
+
+# BODMAS is applied by default in python
+print(10-1+16*6/3)
+
+# powers (Use double multiplication operators)
+print(10 ** 0.5)
+print(10 ** 2)
+
+# Modulus (get the remainder after division)
+print(10 % 2)
+
+# Floor operator  (Remove decimal in numbers)
+# Result is in a whole number
+print(10//3)
+
