c60297e606ec4bffbc586091afb2367ee327228a
developerkorir <caltonkorir45@gmail.com>
2023-10-24 15:22:19 +0300

    If statements extended

--- basics/ifstatements.py
+++ basics/ifstatements.py
@@ -16,3 +16,26 @@ elif temperature > 10:  # (20, 30]
     print("It's a bit cold")
 else:
     print("It's very cold")
+
+# BMI calculator
+height = 1.73
+weight = 60
+
+bmi = weight / height ** 2
+print(f"Your BMI is {bmi}")
+
+if bmi < 18.5:
+    print("Underweight")
+elif bmi < 24.9:
+    print("Normal")
+elif bmi < 29.9:
+    print("Overweight")
+else:
+    print("Obese")
+
+sentence = "Kenya, Uganda and Tanzania"
+
+if "kenya" in sentence.lower():
+    print("Yes, it exists")
+else:
+    print("Does not exist")
