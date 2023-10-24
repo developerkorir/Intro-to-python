50bfc811c8ac12cdbc7c6a5d0f7901e2437ce01a
developerkorir <caltonkorir45@gmail.com>
2023-10-24 08:57:27 +0300

    data organisation

---
+++ basics/lists.py
@@ -0,0 +1,5 @@
+"""
+Lists are used to store multiple items in a single variable.
+Lists are one of 4 built-in data types in Python used to store collections of data,
+the other 3 are Tuple, Set, and Dictionary
+"""
---
+++ basics/while_loops.py
@@ -0,0 +1,19 @@
+"""
+This is a while loop file
+Coding with Mosh
+With the while loop we can execute a set of statements as long as a condition is true.
+"""
+i = 1
+# The following code prints numbers 1 to 5
+while i <= 5:  # i <= 5 is a comparison and <= is a comparison operator
+    print(i)
+    # the following is an assignment similar to i = i + 1
+    i += 1
+
+
+# print a triangle shape using *
+i = 1
+while i <= 5:
+    print(i * "*")  # multiply the string by the value of i
+    i += 1
+
