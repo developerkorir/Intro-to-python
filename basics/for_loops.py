ace4629283df954e630630c092b10682f2992bd3
developerkorir <caltonkorir45@gmail.com>
2023-10-24 09:46:44 +0300

    For Loops

---
+++ basics/for_loops.py
@@ -0,0 +1,8 @@
+"""
+A for loop is used for iterating over a sequence
+"""
+numbers = [1, 2, 3, 4, 5]
+# for every iteration index value increases by 1
+# x hold the value of a number at a particular index
+for x in numbers:
+    print(x)
