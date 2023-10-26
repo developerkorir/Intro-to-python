3f1a996e4c6c87ac9742c9d9ee49b9a324640b8c
developerkorir <caltonkorir45@gmail.com>
2023-10-26 07:52:35 +0300

    Python lists

---
+++ basics/py_lists.py
@@ -0,0 +1,12 @@
+products = ["BMW", "Landrover", "KIA"]
+
+print(products[0])
+print(products[-1])  # print last number in list(use -1)
+
+# for p in products:
+#     print(p)
+
+products.append("Audi")
+print(products)
+
+products.insert(2, "Mercedes")
