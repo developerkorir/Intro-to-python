3621087a29e47cd43cd512776e0174982b9075a9
developerkorir <caltonkorir45@gmail.com>
2023-10-26 15:56:35 +0300

    Python dates

---
+++ python_lists/python_dates.py
@@ -0,0 +1,21 @@
+from datetime import date, datetime, timedelta
+
+# print out today's data
+today = date.today()
+print(today)
+
+dt = datetime.now()
+print(dt)
+
+formatted_date = today.strftime(f"%a, %d %B %Y")
+print(formatted_date)
+
+# check date from the past or future
+_33days_ago = today-timedelta(days=33)
+print(_33days_ago)
+
+_180_days_future = today+timedelta(days=180)
+print(_180_days_future)
+
+# get date from string to date
+
