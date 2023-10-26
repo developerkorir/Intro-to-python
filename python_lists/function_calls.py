b251582c31ee54d9a9c86a1c6ca2edcc9579515d
developerkorir <caltonkorir45@gmail.com>
2023-10-26 15:57:30 +0300

    Python function calls, imports

---
+++ python_lists/function_calls.py
@@ -0,0 +1,18 @@
+# import my_functions
+#
+# my_functions.multiply(2, 4, 6)
+# my_functions.add(10, 20)
+
+from my_functions import add, multiply
+
+add(12, 15)
+
+multiply(10, 11, 12)
+
+# renaming imported modules
+from my_functions import multiply as times
+
+times(10, 11, 12)
+
+
+
