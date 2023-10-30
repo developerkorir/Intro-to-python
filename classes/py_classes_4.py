cec9cfea76b5ab01d0edca6f98bd2d45525f6425
developerkorir <caltonkorir45@gmail.com>
2023-10-30 15:06:50 +0300

    Inheritance, overriding

--- classes/py_classes_4.py
+++ classes/py_classes_4.py
@@ -26,3 +26,21 @@ print(acc1)
 acc2 = Account(balance=580, name="Ibaks", acc_no="0002")  # using named parameters
 acc2.deposit(20)
 print(acc2.acc_balance)
+
+
+# DollarAccount will inherit properties of Account class
+class DollarAccount(Account):
+    def rates(self):
+        return 153.89
+
+    def deposit(self, amount):
+        usd = amount/self.rates()
+        self.acc_balance += usd
+
+
+dc1 = DollarAccount("Kyle", "d-0001", 3200)
+dc1.withdraw(500)
+print(dc1.acc_balance)
+print(dc1.rates())
+dc1.deposit(16000)
+print(round(dc1.acc_balance, 2))  # round off to 2 decimal places
