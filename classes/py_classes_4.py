1795d0d246ca09e6ab3247a1f2383dd563596dea
developerkorir <caltonkorir45@gmail.com>
2023-10-30 14:45:19 +0300

    Printing objects as string

--- classes/py_classes_4.py
+++ classes/py_classes_4.py
@@ -14,10 +14,14 @@ class Account:
         else:
             self.acc_balance -= amount
 
+    def __str__(self):
+        return f"name: {self.acc_name}, acc_no: {self.acc_no}, balance: {self.acc_balance}"
+
 
 acc1 = Account("Jude", "0001", 12900)
 acc1.deposit(100)
 print(acc1.acc_balance)
+print(acc1)
 
 acc2 = Account(balance=580, name="Ibaks", acc_no="0002")  # using named parameters
 acc2.deposit(20)
