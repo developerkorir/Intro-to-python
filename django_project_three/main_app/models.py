052f34f61d2b31ac0bf5b023f4482be866e0fc8e
developerkorir <caltonkorir45@gmail.com>
2023-11-06 17:11:03 +0300

    Project 3 Django

---
+++ django_project_three/main_app/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+# Create your models here.
+class Employee(models.Model):
+    # name, email, dob, salary, disabled
+    name = models.CharField(max_length=40)
+    email = models.EmailField(unique=True)
+    dob = models.DateField(null=True)
+    salary = models.DecimalField(max_digits=6, decimal_places=2)
+    disabled = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
