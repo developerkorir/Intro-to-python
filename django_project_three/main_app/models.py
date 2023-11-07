39b5310df8166633567174c22d683f3b3f3895f6
developerkorir <caltonkorir45@gmail.com>
2023-11-07 14:26:28 +0300

    Automatic fields

--- django_project_three/main_app/models.py
+++ django_project_three/main_app/models.py
@@ -9,6 +9,8 @@ class Employee(models.Model):
     dob = models.DateField(null=True)
     salary = models.DecimalField(max_digits=6, decimal_places=2)
     disabled = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)  # added once during creation
+    updated_at = models.DateTimeField(auto_now=True)  # updates every time update happens
 
     def __str__(self):
         return self.name
