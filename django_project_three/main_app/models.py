6033282307202cb4a5ee31d5f60aebc8477080d5
developerkorir <caltonkorir45@gmail.com>
2023-11-07 14:58:53 +0300

    Uploading media

--- django_project_three/main_app/models.py
+++ django_project_three/main_app/models.py
@@ -7,11 +7,11 @@ class Employee(models.Model):
     name = models.CharField(max_length=40)
     email = models.EmailField(unique=True)
     dob = models.DateField(null=True)
-    salary = models.DecimalField(max_digits=6, decimal_places=2)
+    salary = models.DecimalField(max_digits=7, decimal_places=2)
     disabled = models.BooleanField(default=False)
-    profile = models.ImageField(upload_to="employees")
-    created_at = models.DateTimeField(auto_now_add=True)  # added once during creation
-    updated_at = models.DateTimeField(auto_now=True)  # updates every time update happens
+    profile = models.ImageField(upload_to="employees", null=True)
+    created_at = models.DateTimeField(auto_now_add=True, null=True)  # added once during creation
+    updated_at = models.DateTimeField(auto_now=True, null=True)  # updates every time update happens
 
     def __str__(self):
         return self.name
