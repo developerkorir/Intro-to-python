d4d916bbe24d4fa8004d299760f8924411acd1db
Calton Korir <caltonkorir45@gmail.com>
2023-11-08 14:47:09 +0300

    Default picture in profile

--- django_project_three/main_app/models.py
+++ django_project_three/main_app/models.py
@@ -9,7 +9,7 @@ class Employee(models.Model):
     dob = models.DateField(null=True)
     salary = models.DecimalField(max_digits=7, decimal_places=2)
     disabled = models.BooleanField(default=False)
-    profile = models.ImageField(upload_to="employees", null=True)
+    profile = models.ImageField(upload_to="employees", null=True, default="employees/default.png")
     created_at = models.DateTimeField(auto_now_add=True, null=True)  # added once during creation
     updated_at = models.DateTimeField(auto_now=True, null=True)  # updates every time update happens
 
