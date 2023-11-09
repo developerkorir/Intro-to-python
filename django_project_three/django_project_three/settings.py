6330111c593cf6f071da458e02c0fd63ff9655bb
Calton Korir <caltonkorir45@gmail.com>
2023-11-09 14:56:24 +0300

    404 page

--- django_project_three/django_project_three/settings.py
+++ django_project_three/django_project_three/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+s+a8v7b^qy&7es(1g+wdu#e0hl-9ilso5rthga$%e8c=v8q-6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
